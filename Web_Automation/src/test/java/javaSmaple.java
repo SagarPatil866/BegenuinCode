b37a755a3659d9609d815b2978aefe4017488389
SagarPatil866 <sagarmhaisdhune04@gmail.com>
2025-08-13 11:40:43 +0530

    Create javaSmaple.java
    Please Commit the code United state

---
+++ Web_Automation/src/test/java/javaSmaple.java
@@ -0,0 +1,15 @@
+package Tests;
+
+import Config.ReadProperties;
+import Utils.BasePage;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+
+public class javasample(){
+
+    public static void main(String [] args){
+
+        
+    }
+}
